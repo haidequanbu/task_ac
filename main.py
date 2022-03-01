dd7af31013f8eb0016a858aa902d135ecf802222
haidequanbu <2605965683@qq.com>
2022-03-01 14:46:13 +0800

    测试提交

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVR
+import numpy as np
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def model_liner1():
+    X=np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
+    y=np.dot(X,np.array([1,2]))+3
+    reg=LinearRegression().fit(X,y)
+    # reg.coef_
+    # reg.intercept
+    print(reg.predict(np.array([[3,5]])))
+
+# 暂未完成的实验
+# def model_liner2():
+#     A=np.reshape(np.arange(100),[-1,1])
+#     y=0.3*A+1
+#     reg=LinearRegression().fit(A,y)
+#     print(reg.predict(np.array([1])))
+#
+def model_svr():
+    X=np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
+    y=np.dot(X,np.array([1,2]))+3
+    model = SVR(kernel='rbf',gamma=1,C=100)
+    model.fit(X, y)
+    print(model.predict([[3,4]]))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__=="__main__":
+    model_svr()
+    # model_liner2()
+    # print(sk.__version__)
