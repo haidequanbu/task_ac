80c82bd1c94ee6592f04b74c0c26a253efcc68d1
haidequanbu <2605965683@qq.com>
2022-03-16 19:01:20 +0800

    task3 test

--- task3/train.py
+++ task3/train.py
@@ -39,8 +39,8 @@ def getCorrectNum(probs, targets):
     return correct.item()
 
 
-def train(model, data_loader, optimizer, criterion, max_gradient_norm):
-    file = open(f'log/{time.strftime("%d-%m-%Y")}_train.txt', 'a')
+def train(model, data_loader, optimizer, criterion, max_gradient_norm,epoch):
+    file = open(f'log/{time.strftime("%d-%m-%Y")}_train_{epoch}.txt', 'a')
     model.train()
     DEVICE = model.device
 
@@ -84,8 +84,8 @@ def train(model, data_loader, optimizer, criterion, max_gradient_norm):
     return epoch_time, epoch_loss, epoch_accuracy
 
 
-def validate(model, data_loader, criterion):
-    file = open(f'log/{time.strftime("%d-%m-%Y")}_valid.txt', 'a')
+def validate(model, data_loader, criterion,epoch):
+    file = open(f'log/{time.strftime("%d-%m-%Y")}_valid_{epoch}.txt', 'a')
     model.eval()
     # print(model.device)
     device = model.device
@@ -160,7 +160,7 @@ if __name__ == '__main__':
         # 训练
         print("-" * 50, "Training epoch %d" % (epoch), "-" * 50, file=file)
         print('train EPOCH:', epoch)
-        epoch_time, epoch_loss, epoch_accuracy = train(model, train_loader, optimizer, criterion, max_grad_norm)
+        epoch_time, epoch_loss, epoch_accuracy = train(model, train_loader, optimizer, criterion, max_grad_norm,epoch)
         train_losses.append(epoch_loss)
         print("Training time: {:.4f}s, loss :{:.4f}, accuracy: {:.4f}%".format(epoch_time, epoch_loss,
                                                                                (epoch_accuracy * 100)), file=file)
@@ -170,7 +170,7 @@ if __name__ == '__main__':
         file = open(f'log/{time.strftime("%d-%m-%Y")}_valid_{epoch}.txt', 'w+')
         print("-" * 50, "Validating epoch %d" % (epoch), "-" * 50, file=file)
         print('valid EPOCH:', epoch)
-        epoch_time_dev, epoch_loss_dev, epoch_accuracy_dev = validate(model, dev_loader, criterion)
+        epoch_time_dev, epoch_loss_dev, epoch_accuracy_dev = validate(model, dev_loader, criterion,epoch)
         valid_losses.append(epoch_loss_dev)
         print("Validating time: {:.4f}s, loss: {:.4f}, accuracy: {:.4f}%\n".format(epoch_time_dev, epoch_loss_dev,
                                                                                    (epoch_accuracy_dev * 100)),
